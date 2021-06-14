0ddd81c5bfd92bab855dddf90941f0ca9c9259d7
BennettCritchley <Bennett.critchley1@gmail.com>
2021-06-14 14:48:35 -0600

    changes for grader

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 function addItemsToList() {
-  $(".submit-button").click(function(event){
+  $("#js-shopping-list-form").submit((event) => {
     event.preventDefault();
     alert("Submit!");
-    let item_input = document.getElementById("shopping-list-entry").value;
-    $("ul").append(`<li> <span class="shopping-item">${item_input}</span> <div class="shopping-item-controls"> <button class="shopping-item-toggle"> <span class="check-button">check</span> </button> <button class="shopping-item-delete"> <span class="delete-button">delete</span> </button> </div> </li>`);
+    const item = $(this).find('input[name="shopping-list-entry"]').val();
+    $(".shopping-list").append(
+      `<li> <span class="shopping-item">${item}</span> <div class="shopping-item-controls"> <button class="shopping-item-toggle"> <span class="check-button">check</span> </button> <button class="shopping-item-delete"> <span class="delete-button">delete</span> </button> </div> </li>`
+    );
     $(checkItemsInList);
     $(deleteItemsInList);
   });
-};
+}
 
 function checkItemsInList() {
-  $(".check-button").click(function(event){
+  $(".check-button").click(function (event) {
     event.preventDefault();
     alert("Check!");
-    let item = $(event.currentTarget).parents(".shopping-item-controls").siblings(".shopping-item");
+    let item = $(event.currentTarget)
+      .parents(".shopping-item-controls")
+      .siblings(".shopping-item");
     item.toggleClass("shopping-item__checked");
   });
-};
+}
 
 function deleteItemsInList() {
-  $(".delete-button").click(function(event){
+  $(".delete-button").click(function (event) {
     event.preventDefault();
     alert("Delete!");
-    let item = $(event.currentTarget).parents(".shopping-item-controls").parents("li");
+    let item = $(event.currentTarget)
+      .parents(".shopping-item-controls")
+      .parents("li");
     item.remove();
   });
-};
+}
 
 $(addItemsToList);
 $(checkItemsInList);
 $(deleteItemsInList);
-
-
-
-
-//$(".shopping-item").toggleClass(".shopping-item__checked" + $(event.currentTarget()));
